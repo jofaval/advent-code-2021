7af8094e39c7e5b926cec5bddb7223c4ed6818d4
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-07 16:14:37 +0100

    Answer the 2021-12-03 Part Two challenge

--- 03-12-2021/index.js
+++ 03-12-2021/index.js
@@ -57,6 +57,199 @@ const calculatePowerConsumption = report => {
     return powerConsumption;
 }
 
+let DEBUG;
+DEBUG = true;
+// DEBUG = false;
+
+/**
+ * Implements the logic of finding the most/least common values
+ * 
+ * @param {Array} columns The array of columns to work with
+ * @param {Number} index The current index
+ * @param {String} rawBinary The raw binary string
+ * @param {Boolean} most Is it trying to find the most common one?
+ * @param {String} id The id of the operation
+ * 
+ * @returns {Array}
+ */
+const logic = (columns, index, rawBinary, most, id) => {
+    const bit = rawBinary[index];
+
+    // Increment the value of the column information
+    if (!columns[index]) {
+        if (DEBUG) console.log('push new data to ' + id + ' common columns');
+        columns.push({ '0': 0, '1': 0 });
+
+        if (DEBUG) console.log('increments the bit in ' + id + ' common columns');
+        columns[index][bit]++;
+    } else if (index == 0) {
+        if (DEBUG) console.log('increments the bit in ' + id + ' common columns');
+        columns[index][bit]++;
+    }
+
+    // If the column information doesn't exist, it's created
+    if (index > 0) {
+        const prevCommon = columns[index - 1];
+
+        const prevCommonCondition = most
+            ? prevCommon['0'] > prevCommon['1']
+            : prevCommon['0'] < prevCommon['1'];
+
+        const prevCommonBit = prevCommonCondition ? '0' : '1';
+        const prevBit = rawBinary[index - 1];
+
+        if (prevBit == prevCommonBit) {
+            if (DEBUG) console.log('increments the bit in ' + id + ' common columns');
+            columns[index][bit]++;
+        }
+    }
+
+    return columns;
+}
+
+/**
+ * Calculates the power consumption of the summary given an array of binaries
+ * 
+ * @param {Array} report The array of binaries
+ * 
+ * @returns {Number} The power consumption
+ */
+const calculateLifeSupport = report => {
+    let mostCommonColumns = [];
+    let leastCommonColumns = [];
+
+    // Calculare the result
+    const reportLen = report.length;
+    // const reportLen = 1;
+    /* for (let reportIndex = 0; reportIndex < reportLen; reportIndex++) {
+        const rawBinary = report[reportIndex];
+
+        if (DEBUG) console.log('New raw binary', rawBinary);
+
+        const rawBinaryLen = rawBinary.length;
+        // const rawBinaryLen = 4;
+        for (let rawBinaryIndex = 0; rawBinaryIndex < rawBinaryLen; rawBinaryIndex++) {
+            if (DEBUG) console.log({ rawBinaryIndex, mostCommonColumns, leastCommonColumns, targets: {
+                // most: mostCommonColumns[rawBinaryIndex - 1],
+                // least: leastCommonColumns[rawBinaryIndex - 1]
+            }, current: {
+                // most: mostCommonColumns[rawBinaryIndex],
+                // least: leastCommonColumns[rawBinaryIndex]
+            } });
+
+            // mostCommonColumns = logic(mostCommonColumns, rawBinaryIndex, rawBinary, 'most');
+            // leastCommonColumns = logic(leastCommonColumns, rawBinaryIndex, rawBinary, 'least');
+        }
+    } */
+
+    const cols = report[0].length;
+    
+    // Strings to generate the raw binary string
+    let rawOxygenGeneratorRating = '';
+    let rawC02ScrubberRating = '';
+
+    // Most common columns
+    let tempMostCommonBinaries = report;
+    for (let colIndex = 0; colIndex < cols; colIndex++) {
+        let tempMostCommonBinariesLen = tempMostCommonBinaries.length;
+
+        for (let reportIndex = 0; reportIndex < tempMostCommonBinariesLen; reportIndex++) {
+            const rawBinary = tempMostCommonBinaries[reportIndex];
+            const bit = rawBinary[colIndex];
+
+            if (!mostCommonColumns[colIndex]) mostCommonColumns[colIndex] = { '0': 0, '1': 0 };
+
+            mostCommonColumns[colIndex][bit]++;
+        }
+
+        let mostCommon;
+        let mostCommonBit;
+
+        if (tempMostCommonBinaries.length > 1) {
+            mostCommon = mostCommonColumns[colIndex];
+            if (!mostCommon) mostCommon = { '0': 0, '1': 0 };
+            mostCommonBit = mostCommon['0'] > mostCommon['1'] ? '0' : '1';
+            if (mostCommon['0'] == mostCommon['1']) mostCommonBit = 1;
+        } else {
+            mostCommonBit = tempMostCommonBinaries[0][colIndex]
+        }
+            
+        tempMostCommonBinaries = tempMostCommonBinaries.filter(row => {
+            if (!row[colIndex]) return true;
+            
+            return row[colIndex] == mostCommonBit;
+        });
+
+        rawOxygenGeneratorRating += mostCommonBit;
+    }
+
+    // Least common columns
+    let tempLeastCommonBinaries = report;
+    for (let colIndex = 0; colIndex < cols; colIndex++) {
+        let tempLeastCommonBinariesLen = tempLeastCommonBinaries.length;
+
+        for (let reportIndex = 0; reportIndex < tempLeastCommonBinariesLen; reportIndex++) {
+            const rawBinary = tempLeastCommonBinaries[reportIndex];
+            const bit = rawBinary[colIndex];
+
+            if (!leastCommonColumns[colIndex]) leastCommonColumns[colIndex] = { '0': 0, '1': 0 };
+
+            leastCommonColumns[colIndex][bit]++;
+        }
+
+        let leastCommon;
+        let leastCommonBit;
+
+        if (tempLeastCommonBinaries.length > 1) {
+            leastCommon = leastCommonColumns[colIndex];
+            if (!leastCommon) leastCommon = { '0': 0, '1': 0 };
+            leastCommonBit = leastCommon['0'] < leastCommon['1'] ? '0' : '1';
+            if (leastCommon['0'] == leastCommon['1']) leastCommonBit = 0;
+        } else {
+            leastCommonBit = tempLeastCommonBinaries[0][colIndex]
+        }
+            
+        tempLeastCommonBinaries = tempLeastCommonBinaries.filter(row => {
+            if (!row[colIndex]) return true;
+            
+            return row[colIndex] == leastCommonBit;
+        });
+
+        rawC02ScrubberRating += leastCommonBit;
+    }
+
+    console.log(mostCommonColumns.length, leastCommonColumns.length);
+
+    // Generate the raw binary strings
+    /* const columnsLen = mostCommonColumns.length;
+    for (let columnIndex = 0; columnIndex < columnsLen; columnIndex++) {
+        const mostCommon = mostCommonColumns[columnIndex];
+        // Get the max key and append it
+        const max = mostCommon['0'] > mostCommon['1'] ? '0' : '1';
+        rawOxygenGeneratorRating += max;
+
+        const leastCommon = leastCommonColumns[columnIndex];
+        // Get the min key and append it
+        const min = leastCommon['0'] < leastCommon['1'] ? '0' : '1';
+        rawC02ScrubberRating += min;
+    } */
+
+    // Parse raw binary strings to decimal
+    const oxygenGeneratorRating = parseInt(rawOxygenGeneratorRating, 2);
+    const c02ScrubberRating = parseInt(rawC02ScrubberRating, 2);
+
+    console.log('The submanire life support values are', {
+        rawOxygenGeneratorRating, rawC02ScrubberRating, oxygenGeneratorRating, c02ScrubberRating
+    });
+
+    // Calculate the life support
+    const lifeSupport = oxygenGeneratorRating * c02ScrubberRating;
+
+    console.log('The life support of the submarine is', lifeSupport);
+
+    return lifeSupport;
+}
+
 const report = [
     '00100',
     '11110',
@@ -72,4 +265,5 @@ const report = [
     '01010',
 ]
 
-calculatePowerConsumption(report)
+// calculatePowerConsumption(report)
+calculateLifeSupport(report)
